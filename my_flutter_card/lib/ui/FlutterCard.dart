9b98e037350cd27a477f3dff5a8125d04f0af4fd
AkshayRameshAppDEV <akshay_ramesh@yahoo.com>
2021-12-21 16:18:52 -0600

    Add profile image on top

--- my_flutter_card/lib/ui/FlutterCard.dart
+++ my_flutter_card/lib/ui/FlutterCard.dart
@@ -7,35 +7,50 @@ class FlutterCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
-          appBar: AppBar(
-            title: const Text("Akshay Ramesh Flutter Card"),
-            centerTitle: true,
-            backgroundColor: Colors.redAccent
-          ),
-          body: Center(
-            child: Container(
+      appBar: AppBar(
+          title: const Text("Akshay Ramesh Flutter Card"),
+          centerTitle: true,
+          backgroundColor: Colors.redAccent),
+      body: Center(
+          child: Stack(
+        clipBehavior: Clip.none,
+        alignment: Alignment.topCenter,
+        children: [
+          Container(
               width: 380,
               height: 250,
               decoration: BoxDecoration(
-                color: Colors.redAccent,
-                borderRadius: BorderRadius.circular(20)),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children:  <Widget>[
-                    const Text("Akshay Ramesh", style: TextStyle(fontSize: 30, color: Colors.white)),
-                    const Text("akshayramesh@appdev.com",style: TextStyle(fontSize: 15)),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: const [
-                        Icon(Icons.person),
-                        Text("@AkshayRameshAppDEV", style: TextStyle(fontSize: 15))
-                      ],
-                    )
+                  color: Colors.redAccent,
+                  borderRadius: BorderRadius.circular(20)),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  const Text("Akshay Ramesh",
+                      style: TextStyle(fontSize: 30, color: Colors.white)),
+                  const Text("akshayramesh@appdev.com",
+                      style: TextStyle(fontSize: 15)),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: const [
+                      Icon(Icons.person),
+                      Text("@AkshayRameshAppDEV",
+                          style: TextStyle(fontSize: 15))
+                    ],
+                  )
                 ],
-              )
-            )
-        ),
-      )
-    );
+              )),
+          Positioned(
+            bottom: 130,
+            child: Container(
+                width: 100,
+                height: 100,
+                margin: const EdgeInsets.all(50.0),
+                decoration: BoxDecoration(
+                    color: Colors.black,
+                    borderRadius: BorderRadius.circular(20))),
+          )
+        ],
+      )),
+    ));
   }
 }
