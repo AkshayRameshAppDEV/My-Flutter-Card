b8cf826f8e38acc679900983ce5245316f483947
AkshayRameshAppDEV <akshay_ramesh@yahoo.com>
2021-12-21 13:09:09 -0600

    Add basic container card with box decoration and 3 texts

--- my_flutter_card/lib/ui/FlutterCard.dart
+++ my_flutter_card/lib/ui/FlutterCard.dart
@@ -1,18 +1,35 @@
 import 'package:flutter/material.dart';
 
-class FlutterCard  extends StatelessWidget {
-  const FlutterCard ({Key? key}) : super(key: key);
+class FlutterCard extends StatelessWidget {
+  const FlutterCard({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text("My Flutter Card"),
-          centerTitle: false,
-          backgroundColor: Colors.redAccent,
+    return MaterialApp(
+        home: Scaffold(
+          appBar: AppBar(
+            title: const Text("Akshay Ramesh Flutter Card"),
+            centerTitle: true,
+            backgroundColor: Colors.redAccent
+          ),
+          body: Center(
+            child: Container(
+              width: 380,
+              height: 250,
+              decoration: BoxDecoration(
+                color: Colors.redAccent,
+                borderRadius: BorderRadius.circular(20)),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: const <Widget>[
+                    Text("Akshay Ramesh", style: TextStyle(fontSize: 30)),
+                    Text("akshayramesh@flutter.com",style: TextStyle(fontSize: 20)),
+                    Text("MyText", style: TextStyle(fontSize: 20))
+                ],
+              )
+            )
         ),
-      ),
+      )
     );
   }
 }
