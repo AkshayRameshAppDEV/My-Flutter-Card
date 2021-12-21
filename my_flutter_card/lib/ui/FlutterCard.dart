fc943c69e8fe39fd2ed7c1b2a83fbf4bc2c9ba7b
AkshayRameshAppDEV <akshay_ramesh@yahoo.com>
2021-12-21 16:37:24 -0600

    Refactored

--- my_flutter_card/lib/ui/FlutterCard.dart
+++ my_flutter_card/lib/ui/FlutterCard.dart
@@ -2,6 +2,42 @@ import 'package:flutter/material.dart';
 
 class FlutterCard extends StatelessWidget {
   const FlutterCard({Key? key}) : super(key: key);
+  Container infoCardContainer() {
+    return Container(
+        width: 380,
+        height: 250,
+        decoration: BoxDecoration(
+            color: Colors.redAccent, borderRadius: BorderRadius.circular(20)),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text("Akshay Ramesh",
+                style: TextStyle(fontSize: 30, color: Colors.white)),
+            const Text("akshayramesh@appdev.com",
+                style: TextStyle(fontSize: 15)),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const [
+                Icon(Icons.person),
+                Text("@AkshayRameshAppDEV", style: TextStyle(fontSize: 15))
+              ],
+            )
+          ],
+        ));
+  }
+
+  Positioned profilePicContainer() {
+    return Positioned(
+        bottom: 130,
+        child: Container(
+            width: 100,
+            height: 100,
+            margin: const EdgeInsets.all(50.0),
+            decoration: BoxDecoration(
+                image: const DecorationImage(
+                    image: NetworkImage("https://picsum.photos/200")),
+                borderRadius: BorderRadius.circular(20))));
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -15,41 +51,7 @@ class FlutterCard extends StatelessWidget {
           child: Stack(
         clipBehavior: Clip.none,
         alignment: Alignment.topCenter,
-        children: [
-          Container(
-              width: 380,
-              height: 250,
-              decoration: BoxDecoration(
-                  color: Colors.redAccent,
-                  borderRadius: BorderRadius.circular(20)),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  const Text("Akshay Ramesh",
-                      style: TextStyle(fontSize: 30, color: Colors.white)),
-                  const Text("akshayramesh@appdev.com",
-                      style: TextStyle(fontSize: 15)),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: const [
-                      Icon(Icons.person),
-                      Text("@AkshayRameshAppDEV",
-                          style: TextStyle(fontSize: 15))
-                    ],
-                  )
-                ],
-              )),
-          Positioned(
-              bottom: 130,
-              child: Container(
-                  width: 100,
-                  height: 100,
-                  margin: const EdgeInsets.all(50.0),
-                  decoration: BoxDecoration(
-                      image: const DecorationImage(
-                          image: NetworkImage("https://picsum.photos/200")),
-                      borderRadius: BorderRadius.circular(20))))
-        ],
+        children: [infoCardContainer(), profilePicContainer()],
       )),
     ));
   }
